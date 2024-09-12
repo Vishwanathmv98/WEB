b1afb1011404f757b70444f9e39d798c8e3e0fc7
admin <vishwanathmv98@gmail.com>
2024-09-12 23:43:16 +0530

    frame work

--- web/conftest.py
+++ web/conftest.py
@@ -1,24 +1,27 @@
-import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import platform
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless", action="store_true", help="Run tests in headless mode"
+    )
 
 
 @pytest.fixture(scope='class')
 def web_driver(request):
     chrome_options = Options()
+
+    # Check if --headless option is passed
     if request.config.getoption("--headless"):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920x1080")
 
-    # Detect the OS platform and set driver path accordingly
-    if platform.system() == "Windows":
-        service = Service('C:\\chromedriver.exe')
-    else:
-        service = Service('/usr/local/bin/chromedriver')  # Path for Linux
+    # Set the correct path for chromedriver on Ubuntu
+    service = Service('/usr/local/bin/chromedriver')
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
